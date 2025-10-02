0cf58adb0aa42f03b534eb4867d4e7a96e4d8011
Navid <nh222yr@student.lnu.se>
2025-10-02 21:31:15 +0200

    add basic Electron main process with BrowserWindow

---
+++ src/main/main.js
@@ -0,0 +1,20 @@
+const {app, BrowserWindow} = require('electron')
+const path = require(path)
+
+function createWindow () {
+  const win = new BrowserWindow({
+    width: 700,
+    height: 500,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js') // kan tas bort senare
+    }
+  })
+  // ladda index.html
+  win.loadFile.File(path.resolve(__dirname, '../public/index.html'))
+}
+app.whenReady().then(createWindow)
+
+app.on("window-all-closed", () =>{
+  if(process.platform!=="darwin") app.quit({
+    })
+})
